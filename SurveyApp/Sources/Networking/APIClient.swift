e7e67c102b89b5c9fc39cedf226a97d111d14314
Hasan-codeislife <hasanasim1996@hotmail.com>
2024-11-18 16:48:03 +0100

    Implement base networking layer.
    - Define global network layer.
    - Add network routing.

---
+++ SurveyApp/Sources/Networking/APIClient.swift
@@ -0,0 +1,28 @@
+//
+//  APIClient.swift
+//  SurveyApp
+//
+//  Created by Hassan Personal on 18.11.24.
+//
+
+import Foundation
+
+typealias MyURLResponse = Result<(data: Data, response: HTTPURLResponse), Error>
+
+protocol APIClientProtocol: Sendable {
+    func dataTask(_ request: URLRequest) async -> MyURLResponse
+}
+
+final class APIClientURLSession: APIClientProtocol {
+    func dataTask(_ request: URLRequest) async -> MyURLResponse {
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let response = response as? HTTPURLResponse else {
+                throw MyError.unknown
+            }
+            return .success((data: data, response: response))
+        } catch(let error) {
+            return .failure(error)
+        }
+    }
+}
---
+++ SurveyApp/Sources/Networking/APIManager.swift
@@ -0,0 +1,45 @@
+//
+//  APIManager.swift
+//  SurveyApp
+//
+//  Created by Hassan Personal on 18.11.24.
+//
+
+import Foundation
+
+enum MyError: Error {
+    /// example error handling with no actual implementation
+    case unknown
+    case apiResponseError
+}
+
+protocol ApiManagerProtocol: Sendable {
+    func makeNetworkCall<T: Codable>(router: Routable) async throws -> T
+}
+
+final class ApiManager: ApiManagerProtocol {
+    
+    private let apiClient: APIClientProtocol
+    
+    init(client: APIClientProtocol) {
+        self.apiClient = client
+    }
+    
+    func makeNetworkCall<T: Codable>(router: Routable) async throws -> T {
+        let urlRequest = router.urlRequest
+        let response = await apiClient.dataTask(urlRequest)
+        switch response {
+        case .success((let data, _)):
+            let decoder = JSONDecoder()
+            do {
+                let apiResponse = try decoder.decode(T.self, from: data)
+                return apiResponse
+            } catch let error {
+                throw error
+            }
+            /// You can also implement individual status codes here for various functions (e.g upgrade popup)
+        case .failure(let error):
+            throw error
+        }
+    }
+}
